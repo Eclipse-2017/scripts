4089b6e1a1621fa502e956fbf5ba699d867c6196
Zach Leffke <zleffke@vt.edu>
2017-08-20 02:02:14 -0400

    adding and updating inmarsat recording bash scripts.  Trick was to source GNU Radio environment variables before launching the GNU Radio python file.

--- inmarsat_record_gs3.sh
+++ inmarsat_record_gs3.sh
@@ -2,4 +2,4 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 source /home/odroid/gnuradio/target/setup_env.sh
-python /home/odroid/github/waveforms/inmarsat/inmarsat_record.py --gs-name 'GS1'
+python /home/odroid/github/waveforms/inmarsat/inmarsat_record.py --gs-name 'GS3'
