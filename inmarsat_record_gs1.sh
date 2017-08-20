78408ce0d38e030c57efddaf97193103ccb04482
Zach Leffke <zleffke@vt.edu>
2017-08-20 01:44:12 -0400

    committing inmarsat recording bash script for use with crontab

---
+++ inmarsat_record_gs1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+source /home/odroid/gnuradio/target/setup_env.sh
+python /home/odroid/github/waveforms/inmarsat/inmarsat_record.py
